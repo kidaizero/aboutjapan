5486772fae9b3738d9996cf79e9d156bbf23bef4
yayo256 <>
2018-08-16 14:16:15 +0900

    jinja, css(text-shadow)

--- about_japan.py
+++ about_japan.py
@@ -1,13 +1,19 @@
 # Flask などの必要なライブラリをインポートする
 from flask import Flask, render_template, request, redirect, url_for
-import numpy as np
+import csv
+
+def to_dict(keys, values):
+    return dict(zip(keys, values))
 
 # 自身の名称を app という名前でインスタンス化する
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    with open("infos.csv", "r", encoding="utf-8") as f:
+        infos = [to_dict(["name", "data"], line) for line in csv.reader(f)]
+
+    return render_template('index.html', infos=infos)
 
 if __name__ == '__main__':
     app.debug = True # デバッグモード有効化
